88a84eabec3fb26bcd65c711ec818447041257b0
hakob-yan <hakob9735@gmail.com>
2023-08-14 19:19:54 +0400

    added crystal function

--- src/pages/Home/animate.ts
+++ src/pages/Home/animate.ts
@@ -1,131 +1,98 @@
+import { get } from "./helper";
+import { IParticle, ILine } from "./helper";
+
+interface IParticles {
+  [key: string]: IParticle;
+}
+interface ILines {
+  [key: string]: ILine[];
+}
+
 export default function animate(canvas: HTMLCanvasElement): void {
-    if (!canvas) { return };
-    const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
+  if (!canvas) {
+    return;
+  }
+  const ctx: CanvasRenderingContext2D = canvas.getContext(
+    "2d"
+  ) as CanvasRenderingContext2D;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  window.onresize = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    window.onresize = () => {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    }
-    class Particle {
-        x: number;
-        y: number;
-        size: number;
-        color: string;
-        speedX: number;
-        speedY: number;
-        initialX: number;
-        initialY: number;
-        allowedRadius: number;
-        constructor() {
-            this.x = Math.random() * canvas.width;
-            this.y = Math.random() * canvas.height;
-            this.initialX = this.x;
-            this.initialY = this.y;
-            this.allowedRadius = 70;
-            this.size = Math.random() * 8 + 1;
-            this.color = '#3a0ca3' || `hsl(4,50%,50%)`;
-            this.speedX = 0.2 * (Math.random() - 0.5);
-            this.speedY = 0.2 * (Math.random() - 0.5);
-        }
-        update() {
-            const dx = this.x + this.speedX - this.initialX;
-            const dy = this.y + this.speedY - this.initialY;
-            const distance = Math.sqrt(dx * dx + dy * dy)
+  };
+  const createRandomParticle = get(canvas).createRandomParticle;
+  const createLine = get(canvas).createLine;
 
-            if (distance < this.allowedRadius) {
-                this.x += this.speedX;
-                this.y += this.speedY;
-            }
-            else {
-                const newSpeedX = 0.2 * (Math.random() - 0.5);
-                const newSpeedY = 0.2 * (Math.random() - 0.5);
-                this.speedX = newSpeedX;
-                this.speedY = newSpeedY;
-                this.x += this.speedX;
-                this.y += this.speedY;
-            }
-            this.speedX = this.speedX + this.speedX * 0.05 * Math.abs(this.speedX);
-            this.speedY = this.speedY + this.speedY * 0.05 * Math.abs(this.speedY);
-
-
-        }
-        draw() {
-            ctx.fillStyle = this.color;
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-            ctx.fill()
-
-        }
+  function createCrystal(count: number) {
+    const particlesObj: IParticles = {};
+    const linesObj: ILines = {};
+    for (let i = 0; i < count; ++i) {
+      const randomParticle = createRandomParticle();
+      const x = randomParticle.x;
+      const y = randomParticle.y;
+      particlesObj[`${x}${y}`] = randomParticle;
     }
 
-    class Line {
-        x1: number;
-        y1: number;
-        x2: number;
-        y2: number;
-        constructor(x1: number, y1: number, x2: number, y2: number) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2
-        }
-        update(x1: number, y1: number, x2: number, y2: number) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-        draw() {
-            ctx.beginPath();
-            ctx.strokeStyle = 'white'
-            ctx.lineWidth = 2
-            ctx.moveTo(this.x1, this.y1);
-            ctx.lineTo(this.x2, this.y2);
-            ctx.stroke()
-        }
-    }
+    const particlesArr = Object.values(particlesObj);
 
-    const particlesArr: Particle[] = []
-    const lineArr: Line[] = []
-    for (let i = 0; i < 200; ++i) {
-        particlesArr.push(new Particle());
-
-    }
     for (let i = 0; i < particlesArr.length; ++i) {
-        for (let j = i; j < particlesArr.length; ++j) {
-            const x1 = particlesArr[i].x;
-            const y1 = particlesArr[i].y;
-            const x2 = particlesArr[j].x;
-            const y2 = particlesArr[j].y;
-            const dx = x2 - x1;
-            const dy = y2 - y1;
-            const distance = Math.sqrt(dx * dx + dy * dy)
-            if (distance < 50) {
-                lineArr.push(new Line(x1, y1, x2, y2))
-            }
-
+      for (let j = i; j < particlesArr.length; ++j) {
+        const x1 = particlesArr[i].x;
+        const y1 = particlesArr[i].y;
+        const x2 = particlesArr[j].x;
+        const y2 = particlesArr[j].y;
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < 170) {
+          const line = createLine(x1, y1, x2, y2);
+          if (linesObj[`${x1}${y1}`]) {
+            linesObj[`${x1}${y1}`].push(line);
+          } else {
+            linesObj[`${x1}${y1}`] = [line];
+          }
+          if (linesObj[`${x2}${y2}`]) {
+            linesObj[`${x2}${y2}`].push(line);
+          } else {
+            linesObj[`${x2}${y2}`] = [line];
+          }
         }
+      }
     }
 
-    (function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // ctx.fillStyle = "rgba(0,0,0,0.01)";
-        // ctx.fillRect(0, 0, canvas.width, canvas.height);
-        for (let i = 0; i < particlesArr.length; ++i) {
-            particlesArr[i].update()
-            particlesArr[i].draw()
-            for (let j = i; j < particlesArr.length; ++j) {
-                const dx = particlesArr[i].x - particlesArr[j].x;
-                const dy = particlesArr[i].y - particlesArr[j].y;
-                const distance = Math.sqrt(dx * dx + dy * dy);
-                if (distance < 50) {
-                    lineArr[i].draw()
-                }
-            }
-        }
+    return {
+      particlesObj: particlesObj,
+      linesObj: linesObj,
+    };
+  }
 
-        requestAnimationFrame(animate);
-    })();
+  const { particlesObj, linesObj } = createCrystal(200);
 
+  (function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let key in particlesObj) {
+      particlesObj[key].update();
+      particlesObj[key].draw();
+
+      const newX = particlesObj[key].x;
+      const newY = particlesObj[key].y;
+      linesObj[key].forEach((el) => {
+        el.update(key, newX, newY);
+        el.draw();
+      });
+       
+      const partObj=particlesObj[key];
+      delete particlesObj[key];
+      particlesObj[`${newX}${newY}`]=partObj;
+
+
+      const liObj=linesObj[key];
+      delete linesObj[key];
+      linesObj[`${newX}${newY}`]=liObj;
+    }
+
+    requestAnimationFrame(animate);
+  })();
 }
---
+++ src/pages/Home/helper.ts
@@ -0,0 +1,120 @@
+export interface IParticle {
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+  speedX: number;
+  speedY: number;
+  initialX: number;
+  initialY: number;
+  allowedRadius: number;
+  update: () => void;
+  draw: () => void;
+}
+
+export interface ILine {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  update: (key:string,x: number, y: number) => void;
+  draw: () => void;
+}
+
+interface IGet {
+  createRandomParticle: () => IParticle;
+  createLine: (x1: number, y1: number, x2: number, y2: number) => ILine;
+}
+
+export function get(canvas: HTMLCanvasElement): IGet {
+  const ctx: CanvasRenderingContext2D = canvas.getContext(
+    "2d"
+  ) as CanvasRenderingContext2D;
+  class Particle implements IParticle {
+    x: number;
+    y: number;
+    size: number;
+    color: string;
+    speedX: number;
+    speedY: number;
+    initialX: number;
+    initialY: number;
+    allowedRadius: number;
+    constructor() {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+      this.initialX = this.x;
+      this.initialY = this.y;
+      this.allowedRadius = 90;
+      this.size = 5;
+      this.color = "#3a0ca3" || `hsl(4,50%,50%)`;
+      this.speedX =  (Math.random() - 0.5);
+      this.speedY =  (Math.random() - 0.5);
+    }
+    update() {
+      const dx = this.x + this.speedX - this.initialX;
+      const dy = this.y + this.speedY - this.initialY;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.allowedRadius) {
+        this.x += this.speedX;
+        this.y += this.speedY;
+      } else {
+        const newSpeedX = 0.2 * (Math.random() - 0.5);
+        const newSpeedY = 0.2 * (Math.random() - 0.5);
+        this.speedX = newSpeedX;
+        this.speedY = newSpeedY;
+        this.x += this.speedX;
+        this.y += this.speedY;
+      }
+      this.speedX = this.speedX + this.speedX * 0.05 * Math.abs(this.speedX);
+      this.speedY = this.speedY + this.speedY * 0.05 * Math.abs(this.speedY);
+    }
+    draw() {
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  }
+
+  class Line implements ILine {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    constructor(x1: number, y1: number, x2: number, y2: number) {
+      this.x1 = x1;
+      this.y1 = y1;
+      this.x2 = x2;
+      this.y2 = y2;
+    }
+    update(key: string, x: number, y: number):void {
+      if (key === `${this.x1}${this.y1}`) {
+        this.x1 = x;
+        this.y1 = y;
+      } else if (key === `${this.x2}${this.y2}`) {
+        this.x2 = x;
+        this.y2 = y;
+      }
+    }
+
+    draw():void {
+      ctx.beginPath();
+      ctx.strokeStyle = "blue";
+      ctx.lineWidth = 2;
+      ctx.moveTo(this.x1, this.y1);
+      ctx.lineTo(this.x2, this.y2);
+      ctx.stroke();
+    }
+  }
+
+  return {
+    createRandomParticle(): IParticle {
+      return new Particle();
+    },
+    createLine(x1: number, y1: number, x2: number, y2: number): ILine {
+      return new Line(x1, y1, x2, y2);
+    },
+  };
+}
